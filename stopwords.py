0fb14570f877d9c8d7116a5b96737b4fba5450b2
Sara Ekman <sara.d.ekman@gmail.com>
2018-03-02 19:45:08 +0100

    Create stop words from corpus

---
+++ stopwords.py
@@ -0,0 +1,57 @@
+import os
+from conllu import parse
+from collections import Counter
+
+
+class Stopwords():
+    def __init__(self, list_with_files, number=100):
+        """
+        Creates a list with stopwords INCLUDING punctuation
+        :param list_with_files: list with paths to all conllu files in corpus
+        :param number: Number stopwords to extract, default=100
+        """
+        self.files = list_with_files
+        self.frequency_dict = self.count_frequency(self.files)
+        self.stopword_list = self.create_stopwords(self.frequency_dict, number)
+
+
+    def count_frequency(self, files):
+        """
+        Count frequencies for all lemmas in corpus
+        :param files: list with paths to all conllu files in corpus
+        :return: dict with k=lemma, v=total number occurences in corpus
+        """
+        list_of_words = []
+        for file in files:
+            with open(file, "r") as fin:
+                data = fin.read()
+                for sentence in parse(data):
+                    for word in sentence:
+                        list_of_words.append(word["lemma"])
+            fin.close()
+        counted = Counter(list_of_words)
+        return counted
+
+    def create_stopwords(self, frequency_dict, number):
+        """
+        Pick the n number lemmas for stopword
+        :param frequency_dict: dict with k=lemma, v=total number occurences in corpus
+        :param number: number stopwords to extract, default=100
+        :return: list with n number stopwords (lemmas)
+        """
+        frequency_list = sorted(
+                                frequency_dict,
+                                key=frequency_dict.__getitem__,
+                                reverse=True
+                                )
+        stoplist = frequency_list[:number]
+        return stoplist
+
+
+directory_name = "k_months/"
+list_with_file_strings = []
+for dirpath, dirnames, f_names in os.walk(directory_name):
+    for f in f_names:
+        if f.endswith("conllu"):
+            list_with_file_strings.append(os.path.join(dirpath, f))
+stopword_list = Stopwords(list_with_file_strings).stopword_list
