f67e22855f092b63375b0f7743925d655dab237b
Sara Ekman <sara.d.ekman@gmail.com>
2018-03-02 19:51:02 +0100

    Added docstrings

--- tfidf.py
+++ tfidf.py
@@ -3,8 +3,14 @@ from conllu import parse
 from collections import Counter
 import math
 
+
 class Tfidf():
     def __init__(self, document, list_with_file_names):
+        """
+        :param document: path to document of interest
+        :param list_with_file_names: paths to all conllu formatted docs in corpus, NOT INCLUDING DOCUMENT OF INTEREST
+        :return: prints TF*IDF for alla words in document
+        """
         self.corpus_dict = self.parse_corpus_docs(list_with_file_names)
         self.document = document
         self.tf = self.count_tf(self.document)
@@ -13,6 +19,11 @@ class Tfidf():
         print(self.tfidf)
 
     def parse_corpus_docs(self, list_with_file_names):
+        """
+        extract lemma occurences (Y/N) from conllu files
+        :param list_with_file_names: docs in conllu format
+        :return: dict with k=filename, v=set of all lemmas in the doc
+        """
         self.corpus_dict = {}
         for file_name in list_with_file_names:
             with open(file_name, "r") as fin:
@@ -26,6 +37,11 @@ class Tfidf():
         return self.corpus_dict
 
     def count_tf(self, document):
+        """
+        extracts lemmas and counts Term Frequency from doc of interest
+        :param document: path to doc of interest
+        :return: dict with k=lemma, v=TF
+        """
         with open(document, "r") as fin:
             data = fin.read()
             list_of_words = []
@@ -40,25 +56,37 @@ class Tfidf():
         return self.tf
 
     def count_idf(self, tf, corpus_dict):
-        no_docs = len(corpus_dict) + 1
+        """
+        Counts Inverse Document Frequency for all word in doc of interest
+        :param tf: dict with k=lemma, v=TF for doc of interest
+        :param corpus_dict: dict with k=filename, v=set of all lemmas in the doc
+        :return: dict with k=lemma, v=IDF for doc of interest
+        """
+        num_docs = len(corpus_dict) + 1
         self.idf = {}
         for word, frequency in tf.items():
             doc_occurences = 1
             for doc, word_set in corpus_dict.items():
                 if word in word_set:
                     doc_occurences += 1
-            self.idf[word] = math.log2(no_docs/doc_occurences)
+            self.idf[word] = math.log2(num_docs/doc_occurences)
         return self.idf
 
     def count_tfidf(self, tf, idf):
+        """
+        Counts TF*IDF for each lemma in doc of interest
+        :param tf: dict with k=lemma, v=TF for doc of interest
+        :param idf: dict with k=lemma, v=IDF for doc of interest
+        :return: dict with k=lemma, v=TF*IDF for doc of interest
+        """
         self.tfidf = {}
         for word, value in tf.items():
             self.tfidf[word] = value * idf[word]
         return self.tfidf
 
-document = "t_months/k_2012-03.conllu"
+document = "k_months/k_2013-08.conllu"
 
-directory_name = "t_months/"
+directory_name = "k_months/"
 list_with_file_strings = []
 for dirpath, dirnames, f_names in os.walk(directory_name):
     for f in f_names:
@@ -68,4 +96,3 @@ for dirpath, dirnames, f_names in os.walk(directory_name):
             else:
                 list_with_file_strings.append(os.path.join(dirpath, f))
 my_tfidf_model = Tfidf(document, list_with_file_strings)
-
