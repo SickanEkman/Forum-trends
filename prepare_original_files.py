399b6ce58507f041710b616b29156d10c8dba1ac
Sara Ekman <sara.d.ekman@gmail.com>
2018-03-02 19:52:34 +0100

    Excluded unnecessary step

--- prepare_original_files.py
+++ prepare_original_files.py
@@ -6,13 +6,13 @@ import os.path
 
 def create_work_files(og_file):
     """
-    :param og_file: the original forum json-file with no modifications
-    :return: writes dict to output file, example names: data_a.json, data_k.json
+    :param og_file: an original forum json-file with no modifications
+    :return: send a dict for each file to function "separate_each_month"
     """
     data = collections.defaultdict(list)
     with open(og_file, "r") as fin:
-        forum = json.load(fin)
-        for dialogs in forum["dialogs"]:
+        og_forum = json.load(fin)
+        for dialogs in og_forum["dialogs"]:
             dialog_text = dialogs["content_text"]
             dialog_text = dialog_text.replace("\n", " ")
             dialog_text = dialog_text.replace("  ", " ")
@@ -27,46 +27,29 @@ def create_work_files(og_file):
                 comment_date = comment_date[0:7]
                 data[comment_date].append(comment_text)
     fin.close()
-    outfile_name = "data_" + og_file[6] + ".json"
-    with open(outfile_name, "w") as fout:
-        json.dump(data, fout)
-    fout.close()
+    separate_each_month(data, og_file[6])
 
 
-def separate_each_month(mod_file):
+def separate_each_month(data_dict, file_name):
     """
-    :param mod_file: modified forum file with date as dict key and list with texts as value
+    :param data_dict: k=date, v=list with texts
+    :param file_name: the identifier from original files, example= "a", "k", ...
     :return: files for each month, with all text joined, example path: "m_months/m_2016-01.txt"
     """
-    subdir = mod_file[5] + "_months"
+    subdir = file_name + "_months"
     try:
         os.mkdir(subdir)
     except FileExistsError:
-        print("Directory '%s' already exists." % mod_file)
-    with open(mod_file, "r") as fin:
-        forum_text = json.load(fin)
-        for date, texts in forum_text.items():
-            big_str = " ".join(texts)
-            outfile_name = mod_file[5] + "_" + date + ".txt"
-            with open(os.path.join(subdir, outfile_name), "w") as fout:
-                fout.write(big_str)
-            fout.close()
-    fin.close()
+        print("Directory '%s' already exists." % subdir)
+    for date, texts in data_dict.items():
+        big_str = " ".join(texts)
+        outfile_name = file_name + "_" + date + ".txt"
+        with open(os.path.join(subdir, outfile_name), "w") as fout:
+            fout.write(big_str)
+        fout.close()
 
 
-def mod_og_files():
-    forums = original_filenames.forums  # haven't tested program after moving file names to separate file,
-    # change if needed
+if __name__ == "__main__":
+    forums = original_filenames.forums
     for forum in forums:
         create_work_files(forum)
-
-
-def mod_work_files():
-    data_files = ["data_a.json",
-                  "data_c.json",
-                  "data_k.json",
-                  "data_l.json",
-                  "data_m.json",
-                  "data_s.json"]
-    for data_file in data_files:
-        separate_each_month(data_file)
