0fca0c9c77fa7c19005c876c4b477faa3e83787d
Sara Ekman <sara.d.ekman@gmail.com>
2018-03-12 17:53:19 +0100

    refactoring

--- tag_text.py
+++ tag_text.py
@@ -50,24 +50,20 @@ class Model:
         return output
 
 
-# English: "english-ud-2.0-170801.udpipe"
-# Swedish: "swedish-ud-2.0-170801.udpipe"
-training_file = "swedish-ud-2.0-170801.udpipe"
-directory_name = "k_months/"
-
-model = Model(training_file)
-for dirpath, dirnames, filenames in os.walk(directory_name):
-    file_list = [os.path.join(dirpath, filename) for filename in filenames]
-    for f in file_list:
-        with open(f, "r") as fin:
-            forum_text = fin.read()
-            sentences = model.tokenize(forum_text)
-            for s in sentences:
-                model.tag(s)
-            conllu = model.write(sentences, "conllu")
-            outfile_name = f[9:18] + ".conllu"
-            print(dirpath + outfile_name)
-            with open(os.path.join(dirpath, outfile_name), "w") as fout:
-                fout.write(conllu)
-            fout.close()
-        fin.close()
+def create_tagged_files(training_file, directory_name):
+    model = Model(training_file)
+    for dirpath, dirnames, filenames in os.walk(directory_name):
+        file_list = [os.path.join(dirpath, filename) for filename in filenames]
+        for f in file_list:
+            with open(f, "r") as fin:
+                forum_text = fin.read()
+                sentences = model.tokenize(forum_text)
+                for s in sentences:
+                    model.tag(s)
+                conllu = model.write(sentences, "conllu")
+                outfile_name = f[9:18] + ".conllu"
+                print(dirpath + outfile_name)
+                with open(os.path.join(dirpath, outfile_name), "w") as fout:
+                    fout.write(conllu)
+                fout.close()
+            fin.close()
