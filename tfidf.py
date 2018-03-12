99ebb95e938a1816bd1e8c53629e114382acfffd
Sara Ekman <sara.d.ekman@gmail.com>
2018-03-12 17:55:02 +0100

    create-object-function moved to main

--- tfidf.py
+++ tfidf.py
@@ -1,4 +1,3 @@
-import os
 from conllu import parse
 from collections import Counter
 import math
@@ -16,11 +15,12 @@ class Tfidf():
         self.tf = self.count_tf(self.document)
         self.idf = self.count_idf(self.tf, self.corpus_dict)
         self.tfidf = self.count_tfidf(self.tf, self.idf)
-        print(self.tfidf)
+        print("TF*IDF:")
+        print(sorted(self.tfidf.items(), key=lambda t:t[1], reverse=True))
 
     def parse_corpus_docs(self, list_with_file_names):
         """
-        extract lemma occurences (Y/N) from conllu files
+        extract lemma occurrences (Y/N) from conllu files
         :param list_with_file_names: docs in conllu format
         :return: dict with k=filename, v=set of all lemmas in the doc
         """
@@ -57,7 +57,7 @@ class Tfidf():
 
     def count_idf(self, tf, corpus_dict):
         """
-        Counts Inverse Document Frequency for all word in doc of interest
+        Counts Inverse Document Frequency for all words in doc of interest
         :param tf: dict with k=lemma, v=TF for doc of interest
         :param corpus_dict: dict with k=filename, v=set of all lemmas in the doc
         :return: dict with k=lemma, v=IDF for doc of interest
@@ -83,16 +83,3 @@ class Tfidf():
         for word, value in tf.items():
             self.tfidf[word] = value * idf[word]
         return self.tfidf
-
-document = "k_months/k_2013-08.conllu"
-
-directory_name = "k_months/"
-list_with_file_strings = []
-for dirpath, dirnames, f_names in os.walk(directory_name):
-    for f in f_names:
-        if f.endswith("conllu"):
-            if f in document:
-                pass
-            else:
-                list_with_file_strings.append(os.path.join(dirpath, f))
-my_tfidf_model = Tfidf(document, list_with_file_strings)
