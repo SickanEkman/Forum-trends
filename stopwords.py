76f3c8d6dc2d78e66e890fbe53aa73c1dd5e020d
Sara Ekman <sara.d.ekman@gmail.com>
2018-03-13 18:58:29 +0100

    added pickle

--- stopwords.py
+++ stopwords.py
@@ -1,23 +1,25 @@
 from conllu import parse
 from collections import Counter
+import pickle
 
 
 class Stopwords():
-    def __init__(self, list_with_files, number=100):
+    def __init__(self, list_with_files, directory_name, forum_nick):
         """
-        Creates a list with stopwords INCLUDING punctuation
+        Create a list with type frequencies INCLUDING punctuations
         :param list_with_files: list with paths to all conllu files in corpus
-        :param number: Number stopwords to extract, default=100
+        :param directory_name: directory name, ending with "/"
+        :param forum_nick: name of forum, to use for file naming
         """
-        self.files = list_with_files
-        self.frequency_dict = self.count_frequency(self.files)
-        self.stopword_list = self.create_stopwords(self.frequency_dict, number)
+        self.outfile_name = directory_name + forum_nick + "_stoplist.pkl"
+        self.frequency_list = self.count_frequency(list_with_files)
+        self.pickle_frequency_list(self.frequency_list, self.outfile_name)
 
     def count_frequency(self, files):
         """
         Count frequencies for all lemmas in corpus
         :param files: list with paths to all conllu files in corpus
-        :return: dict with k=lemma, v=total number occurences in corpus
+        :return: list with all types in corpus, sorted descending by frequency
         """
         list_of_words = []
         for file in files:
@@ -28,19 +30,21 @@ class Stopwords():
                         list_of_words.append(word["lemma"])
             fin.close()
         counted = Counter(list_of_words)
-        return counted
-
-    def create_stopwords(self, frequency_dict, number):
-        """
-        Pick the n number lemmas for stopword
-        :param frequency_dict: dict with k=lemma, v=total number occurences in corpus
-        :param number: number stopwords to extract, default=100
-        :return: list with n number stopwords (lemmas)
-        """
         frequency_list = sorted(
-                                frequency_dict,
-                                key=frequency_dict.__getitem__,
-                                reverse=True
-                                )
-        stoplist = frequency_list[:number]
-        return stoplist
+            counted,
+            key=counted.__getitem__,
+            reverse=True
+        )
+        return frequency_list
+
+    def pickle_frequency_list(self, frequency_list, outfile_name):
+        """
+        pickle the frequency list and write it to file
+        :param frequency_list: list with all types in corpus, sorted descending by frequency
+        :param outfile_name: name of output file, including path
+        :return: file with pickled frequency list
+        """
+        with open(outfile_name, "wb") as fout:
+            pickle.dump(frequency_list, fout)
+        fout.close()
+
